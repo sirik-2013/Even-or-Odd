200d3fc176a4a3cff0159f42d0c1fd6e8eeba07e
siri <sirikannikanti@gmail.com>
2024-05-13 13:59:42 -0500

    commit complete

---
+++ src/in/siridev/EvenOrOdd.java
@@ -0,0 +1,13 @@
+package in.siridev;
+
+public class EvenOrOdd {
+	
+	public static void main(String...args) {
+	int a =2;
+	if(a%2 ==0) {
+	System.out.println("it is a Even Number");
+   }else {
+	System.out.println("it is a Odd Number");
+   }
+  }
+}
